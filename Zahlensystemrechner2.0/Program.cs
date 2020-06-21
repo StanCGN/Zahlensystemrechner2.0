41f8c02d644bc0bda09b2eb2f10bbe33036d002c
Noemi Elejalde Glaeser <noemi.e@schule.koeln>
2020-06-21 20:30:10 +0200

    Binär in Dezimalzahl

--- Zahlensystemrechner2.0/Program.cs
+++ Zahlensystemrechner2.0/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Zahlensystemrechner2._0
 {
@@ -6,7 +9,35 @@ namespace Zahlensystemrechner2._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int BinärZahl = Convert.ToInt32(Console.ReadLine());
+            Program.BinärinDez(0,BinärZahl);
+
+        }
+        static void BinärinDez(int Erg, int BinärZahl)
+        {
+            //int length = 0;
+            //int z;
+            var BinärStack = new Stack<int>(32);
+            do
+            {
+                BinärStack.Push(BinärZahl % 10);
+                BinärZahl /= 10;
+            } while (BinärZahl != 0);
+            int[] BinärArray = BinärStack.ToArray();
+            int length = BinärArray.Length;
+            for (int y = length; y > 0; y--)
+            {
+                for (int x = 0; x < length-1; x++)
+                {
+                   int i = BinärArray[y-1];
+                    Erg += i * 2 ^ x;
+
+                }
+            }
+            Console.WriteLine("länge ist" + length);
+            Console.WriteLine(Erg);
+            Console.ReadLine();
+            //return Erg;
         }
     }
 }
